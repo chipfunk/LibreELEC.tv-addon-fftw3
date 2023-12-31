f4631e8674e3bb66202a58b70bac92b5bf1e632d
Christian Döring <chipfunk@gmx.de>
2023-12-31 14:07:11 +0100

    Fix addon-build, add license-header and maintainer

--- package.mk
+++ package.mk
@@ -1,20 +1,5 @@
-################################################################################
-#      This file is part of LibreELEC - https://libreelec.tv
-#      Copyright (C) 2009-2016 Lukas Rusak (lrusak@libreelec.tv)
-#
-#  LibreELEC is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, either version 2 of the License, or
-#  (at your option) any later version.
-#
-#  LibreELEC is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-#
-#  You should have received a copy of the GNU General Public License
-#  along with LibreELEC.  If not, see <http://www.gnu.org/licenses/>.
-################################################################################
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="fftw3"
 PKG_VERSION="3.3.10"
@@ -24,6 +9,7 @@ PKG_LICENSE="Proprietary"
 PKG_SITE="http://www.fftw.org"
 PKG_URL="http://www.fftw.org/fftw-${PKG_VERSION}.tar.gz"
 PKG_SHA256_CHECKSUM="56c932549852cddcfafdab3820b0200c7742675be92179e59e6215b340e26467"
+PKG_MAINTAINER="chipfunk" # Full name or forum/GitHub nickname, if you want to be identified as the addon maintainer
 # PKG_SOURCE_DIR="${PKG_NAME}-${PKG_VERSION}"
 PKG_DEPENDS_TARGET="glibc"
 PKG_SECTION="service/system"
@@ -33,11 +19,15 @@ PKG_TOOLCHAIN="configure" # or one of auto, meson, cmake, cmake-make, configure,
 
 PKG_CONFIGURE_OPTS_TARGET="--disable-fortran"
 
+PKG_LIBNAME="libfftw3.so"
+PKG_LIBPATH=".libs/${PKG_LIBNAME}"
+PKG_LIBVAR="FFTW3_LIB"
+
 PKG_IS_ADDON="yes"
-PKG_ADDON_NAME="fftw3"
-PKG_ADDON_TYPE="xbmc.service.library"
+PKG_ADDON_NAME="${PKG_NAME}"
+PKG_ADDON_TYPE="xbmc.service"
 
 addon() {
-  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}/usr/lib
-  cp -r ${PKG_INSTALL}/usr/lib/libfftw3* ${ADDON_BUILD}/${PKG_ADDON_ID}/usr/lib
+  mkdir -p ${ADDON_BUILD}/${PKG_ADDON_ID}
+  cp ${PKG_INSTALL}/usr/lib/libfftw3* ${ADDON_BUILD}/${PKG_ADDON_ID}
 }
